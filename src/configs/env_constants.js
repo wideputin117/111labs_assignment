e7c603f3c74c7a16735511e88b83c454062aa047
wideputin117 <takermanish7@gmail.com>
2025-07-27 23:04:28 +0530

    new changes

--- src/configs/env_constants.js
+++ src/configs/env_constants.js
@@ -1,19 +1,14 @@
 import { configDotenv } from "dotenv";
 configDotenv();
 
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const GOOGLE_REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
+ 
 
 const SECONDS_PER_MINUTE = 60;
 const MINUTES_PER_HOUR = 60;
 const HOURS_PER_DAY = 24;
 const MILLISECONDS_PER_SECOND = 1000;
 
-
-export const OAUTH_EXCHANGE_EXPIRY =
-    15 * SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND; // 15 minutes
-
+ 
 export const ACCESS_TOKEN_EXPIRY =
     1 *
     HOURS_PER_DAY *
