d1bd9b507937b4ea59c6c98079006b7b55016b4a
Manish <manishgupta@pearlorganisation.com>
2025-07-28 17:09:11 +0530

    added routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -3,7 +3,7 @@ import express from 'express'
 import { authenticateToken, verifyPermission } from "../middlewares/authMiddleware.js"
 const router = express.Router()
 
-router.route(`/company`).post(authenticateToken,verifyPermission('ADMIN'),add_company).get(get_companies)
+router.route(`/company`).post(authenticateToken,verifyPermission('ADMIN'),add_company).get(authenticateToken,get_companies)
 router.route(`/companies/:companyId`).patch(update_company_detail).delete(delete_company).get(get_single_company)
 
 /** for drivers operations */
